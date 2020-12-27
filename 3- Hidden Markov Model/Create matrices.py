c23e6669d38351296f83121d77437ed2f0eda562
Théo Demessance <49204997+TheoDemessance@users.noreply.github.com>
2020-12-27 16:51:36 +0100

    Update Create matrices.py

--- 3- Hidden Markov Model/Create matrices.py
+++ 3- Hidden Markov Model/Create matrices.py
@@ -1,33 +1,39 @@
 def create_matrix_transition(self, current = None, visited = None):
-    if not current:
-        current = self.root
-    
-    if not visited:
-        visited = []
+        if not current:
+            current = self.root
         
-    for i in range(len(current.children)):
-        if(current.children[i][2] not in visited):
-            self.matrice_transition.loc[current.text, current.children[i][2].text] = round(current.children[i][3], 2)
-            visited.append(current)
-            self.create_matrix_transition(current.children[i][2], visited) 
+        if not visited:
+            visited = []   
             
-    self.matrice_transition = self.matrice_transition.fillna(0).astype('float')
-    #self.matrice_transition = self.matrice_transition.to_numpy(dtype = 'float')
+        for i in range(len(current.children)):
+            #We group the transitions having the same "destination" node, to fill the matrix with the transition probability
+            listt = self.group_transition(current)
+            for c in listt:
+                self.matrice_transition.loc[current.text, c[0][2].text] = round(c[0][3], 2)
+            visited.append(current)
+                
+        for i in range(len(current.children)):
+            #we iterate in all the children to fill all the matrix
+            if(current.children[i][2] not in visited):
+                self.create_matrix_transition(current.children[i][2], visited) 
+                
+        self.matrice_transition = self.matrice_transition.fillna(0).astype('float')
         
-
-def create_matrix_emission(self, current = None, visited = None):
-    if not current:
-        current = self.root
-    
-    if not visited:
-        visited = []
+    def create_matrix_emission(self, current = None, visited = None):
+        if not current:
+            current = self.root
         
-    for i in range(len(current.children)):
-        if(current.children[i][2] not in visited):
+        if not visited:
+            visited = []
+            
+        for i in range(len(current.children)):
             self.matrice_emission.loc[current.text, current.children[i][0]] = round(current.children[i][1], 2)
             visited.append(current)
-            self.create_matrix_emission(current.children[i][2], visited) 
-            
-    self.matrice_emission = self.matrice_emission.fillna(0).astype('float')
-    self.matrice_emission = self.matrice_emission.reindex(sorted(self.matrice_emission.columns), axis=1)
-    #self.matrice_emission = self.matrice_emission.to_numpy(dtype = 'float')
+        
+        for i in range(len(current.children)):
+            if(current.children[i][2] not in visited):
+                self.create_matrix_emission(current.children[i][2], visited) 
+        
+                
+        self.matrice_emission = self.matrice_emission.fillna(0).astype('float')
+        self.matrice_emission = self.matrice_emission.reindex(sorted(self.matrice_emission.columns), axis=1)
